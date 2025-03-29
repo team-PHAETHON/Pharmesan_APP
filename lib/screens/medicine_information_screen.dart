8c32ef89208eec96f22569aaf7a9fecf9cea9965
Redish03 <pluto0303@pukyong.ac.kr>
2025-03-29 14:38:34 +0000

    refactor: call API at medicine_information_screen

--- lib/screens/medicine_information_screen.dart
+++ lib/screens/medicine_information_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/data/medicine_data.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 const double leftPadding = 30;
 const double topPadding = 80;
@@ -10,6 +13,10 @@ const double headerWidth = 300;
 const int mainThemeColor = 0xff578FCA;
 
 class MedicineInformationScreen extends StatefulWidget {
+  final String medicineName;
+
+  MedicineInformationScreen({required this.medicineName});
+
   @override
   _MedicineInformationScreenState createState() =>
       _MedicineInformationScreenState();
@@ -19,16 +26,58 @@ class _MedicineInformationScreenState extends State<MedicineInformationScreen> {
   final String medicineImageURL =
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBgjfXhLFiTbASWw6Wz6o3ySlPhHPJdWis8A&s";
 
-  final String medicineName = "약1";
-  final String medicineDescription =
-      "이 약은 티눈, 사마귀 치료에 사용됩니다.\n\n 사용방법 : 아침 저녁으로~~";
+  late Future<MedicineData?> _medicineData;
+
+  @override
+  void initState() {
+    super.initState();
+    _medicineData = fetchMedicineData(widget.medicineName);
+  }
+
+  Future<MedicineData?> fetchMedicineData(String name) async {
+    final response = await http.get(
+      Uri.parse(
+        'https://practicespringboot-tdxsp.run.goorm.site/medicine/search?name=$name',
+      ),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      if (jsonData != null) {
+        return MedicineData.fromJson(jsonData);
+      }
+    }
+    return null;
+  }
+
+  // final String medicineName = "약1";
+  // final String medicineDescription =
+  //     "이 약은 티눈, 사마귀 치료에 사용됩니다.\n\n 사용방법 : 아침 저녁으로~~";
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(leftPadding, topPadding, rightPadding, bottomPadding),
+      body: FutureBuilder<MedicineData?>(
+        future: _medicineData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("데이터를 불러올 수 없습니다"));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return Center(child: Text("검색 결과가 없습니다."));
+          }
+          final data = snapshot.data!;
+
+          return Padding(
+            padding: EdgeInsets.fromLTRB(
+              leftPadding,
+              topPadding,
+              rightPadding,
+              bottomPadding,
+            ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
