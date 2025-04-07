ccffcb284fce4d42e6066aa32a1dc6e20052a2e3
Redish03 <pluto0303@pukyong.ac.kr>
2025-04-07 14:28:42 +0000

    feat: create search_result_medicie_cataolg_screen.dart and init

---
+++ lib/screens/search_result_medicine_catalog_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchResultMedicineCatalogScreen extends StatefulWidget {
+  @override
+  _SearchResultMedicineCatalogScreen createState() =>
+      _SearchResultMedicineCatalogScreen();
+}
+
+class _SearchResultMedicineCatalogScreen
+    extends State<SearchResultMedicineCatalogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
