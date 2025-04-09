a05990e616fa573afb4ef36244bbc9cfa6753f97
Redish03 <pluto0303@pukyong.ac.kr>
2025-04-09 14:05:20 +0000

    feat: make Medicine Card to return Card type

--- lib/widget/medicine_card.dart
+++ lib/widget/medicine_card.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/data/medicine_data.dart';
 
 class MedicineCard extends StatelessWidget {
-  final Medicine medicine;
-  const MedicineCard({
-    super.key,
-    required this.medicine,
-  });
+  final MedicineData medicine;
+  const MedicineCard({super.key, required this.medicine});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(medicine.name),
-        subtitle: Text(medicine.dosage),
+        title: Text(medicine.name ?? ''),
+        subtitle: Text(medicine.description ?? ''),
       ),
     );
   }
